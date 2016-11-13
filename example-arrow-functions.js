88870d749cf8e887e6a2057559f48ebb2adb483b
Sean <seandunn.co@gmail.com>
2016-11-13 08:17:50 -0500

    Initial commit

---
+++ example-arrow-functions.js
@@ -0,0 +1,25 @@
+// var names = ['Sean', 'Tyler', 'Doug'];
+//
+// names.forEach(function (name){
+//   console.log('forEach', name);
+// });
+//
+// names.forEach((name) => {
+//   console.log('arrowFunc', name);
+// });
+//
+// names.forEach((name) => console.log(name));
+
+// var returnMe = (name) => name + '!';
+// console.log(returnMe('Sean'));
+
+// var person = {
+//   name: 'Sean',
+//   greet: function() {
+//     names.forEach((name) => {
+//       console.log(this.name + ' says hi to ' + name)
+//     });
+//   }
+// };
+//
+// person.greet();
